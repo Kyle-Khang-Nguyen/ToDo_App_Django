a8925025e148faa3cf8411bd0d3b46c2cacb2ab8
DawningDew <kyle.nguyen08@yahoo.com>
2026-08-29 17:26:31 +0200

    add .gitignore and .env

--- TestToDo/settings.py
+++ TestToDo/settings.py
@@ -11,19 +11,17 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv  # Paket importieren
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Pfad zur .env-Datei festlegen und laden
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
+# Den Key jetzt dynamisch aus der .env-Datei auslesen
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(wmt7zwjs!x8*wv$rv#h70nn21fv-f75y7o)yo1us31*q!%3q)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Optional: Auch DEBUG über die .env steuern
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
