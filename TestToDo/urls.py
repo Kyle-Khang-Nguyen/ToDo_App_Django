e3d667a0baba4a27a201cd803609760758526027
DawningDew <kyle.nguyen08@yahoo.com>
2026-08-30 11:10:24 +0200

    create task

--- TestToDo/urls.py
+++ TestToDo/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include  # Importiere include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('to_do.urls')),  # Füge die URLs der to_do App hinzu
 ]
--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ToDo
 
 # Create your views here.
+def task_list(request):
+    tasks = ToDo.objects.all()
+    return render(request, 'to_do.html', {'tasks': tasks})
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        deadline = request.POST.get('deadline')
+
+        ToDo.objects.create(title=title, description=description, deadline=deadline)
+        return redirect('task_list')
+
+    return render(request, 'create_task.html')
+
+def delete_task(request, task_id):
+    task = ToDo.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
